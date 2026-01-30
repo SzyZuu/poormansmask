45991d5c0decb620607bfbce21b2d00ca6561195
Szymon Z <szurawski@ethz.ch>
2026-01-31 00:03:02 +0100

    feat: Item resource to create items ezzz

---
+++ scripts/ItemResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Godot;
+using poormansmask.scripts.enums;
+
+namespace poormansmask.scripts;
+
+[GlobalClass]
+public partial class ItemResource : Resource
+{
+    [Export]
+    private float[] _statImprovements = new float[Enum.GetValues(typeof(StatImprovements)).Length];
+    
+    [Export]
+    private Texture2D _itemSprite;
+    
+    [Export]
+    private string _itemName;
+    
+    [Export]
+    private string _itemDescription;
+    
+    [Export]
+    private Rarities _itemRarity;
+    
+    public float[] GetStatImprovements() =>  _statImprovements;
+}
---
+++ scripts/enums/Rarities.cs
@@ -0,0 +1,13 @@
+﻿namespace poormansmask.scripts.enums;
+
+public enum Rarities
+{
+    COMMON,
+    UNCOMMON,
+    RARE,
+    VERY_RARE,
+    RAREZ,
+    EPIC,
+    LEGENDARY,
+    RAREST // only one (GAMBLING)
+}
