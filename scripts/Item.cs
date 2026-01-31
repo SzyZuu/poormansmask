627b36eac99559c51273e1708a673062c01e76bb
SzyZu <skwarek116@gmail.com>
2026-01-31 12:49:36 +0100

    feat: character autopickup

--- scripts/Item.cs
+++ scripts/Item.cs
@@ -1,7 +1,22 @@
 using Godot;
 using System;
+using poormansmask.scripts;
 
 public partial class Item : Area2D
 {
-	[Export] public Resource itemData;
+	[Export] public ItemResource ItemData;
+
+	override public void _Ready()
+	{
+		BodyEntered += OnItemEnter;
+	}
+
+	private void OnItemEnter(Node2D body)
+	{
+		GD.Print(body);
+		if (body.HasMethod("ItemPickUp"))
+			body.Call("ItemPickUp", ItemData);
+		
+		QueueFree();
+	}
 }
